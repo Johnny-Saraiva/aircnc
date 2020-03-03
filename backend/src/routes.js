807d082ce8b6c342d64394c420fddd9be54ffb3d
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-03-02 21:42:15 -0300

    create and config user sesssion

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,7 @@
 const express = require('express');
-
+const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
-routes.get('/users', (req, res) => {
-  return res.json({message: 'Hello World'});
-});
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes;
