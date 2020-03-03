993ccfe2b1188f8816532bf76a32a6ca5588cafe
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-03-03 14:46:41 -0300

    config axios to submit users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+
+  const [ email, setEmail ] = useState('');
+
+  async function handleSubmit(event){
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+    
+  };
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnc"/>
@@ -14,9 +29,15 @@ function App() {
           <strong>talentos</strong> para sua empresa.
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
-          <input type="email" id="email" placeholder="Seu melhor e-mail" />
+          <input 
+            id="email" 
+            type="email" 
+            placeholder="Seu melhor e-mail" 
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
           <button className="btn" type="submit">Entrar</button>
         </form>
       </div>
