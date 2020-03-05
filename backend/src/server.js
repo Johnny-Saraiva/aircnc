301cff0ab5590c3112572a5e95ebff750c262c21
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-03-05 15:40:52 -0300

    add socketio lib

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-xin74.mongodb.net/aircnc?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -17,4 +22,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
