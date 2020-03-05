cf19898e9e6357e22b5011c2d2b01b08cb57cdcc
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-03-05 15:24:57 -0300

    Config list page to switch to book and save data

--- mobile/src/pages/Book.js
+++ mobile/src/pages/Book.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, TextInput, TouchableOpacity, AsyncStorage, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Alert,TextInput, TouchableOpacity, AsyncStorage, Text } from 'react-native';
+
+import api from '../services/api';
 
 export default function Book({ navigation }) {
 
@@ -7,7 +9,23 @@ export default function Book({ navigation }) {
 
   const id = navigation.getParam('id');
 
-  function handleSubmit() {}
+  async function handleSubmit() {
+    const user_id = await AsyncStorage.getItem('user');
+
+    await api.post(`/spots/${id}/bookings`, {
+      date
+    }, {
+      headers: { user_id }
+    })
+
+    Alert.alert('Solicitação de reserva enviada.');
+
+    navigation.navigate('List');
+  }
+
+  function handleCancel() {
+    navigation.navigate('List');
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -26,7 +44,7 @@ export default function Book({ navigation }) {
         <Text style={styles.buttonText}>Solicitar reserva</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={handleSubmit} style={[styles.button, styles.cancelButton]}>
+      <TouchableOpacity onPress={handleCancel} style={[styles.button, styles.cancelButton]}>
         <Text style={styles.buttonText}>Cancelar</Text>
       </TouchableOpacity>
     </SafeAreaView>
